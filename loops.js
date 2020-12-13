4efd5fcaf7451b749ff0e472ff95ad5b0a870aa4
Gabrielle Piacentile <gabriellepiacentile98@gmail.com>
2020-12-13 19:52:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,9 @@
-var array
 function forLoop (array){
-for (let i = 0, i<26, i++){
-  console.log ('"I am ${i} strange loop${i === 0 ?'' : 's'}."')
-}
+  for (let i = 0, i<26, i++){
+    if (i === 1){
+      console.log ('"I am ${i} strange loop.')
+    } else {
+      console.log ('"I am ${i} strange loops."')
+    }
+  }
 }
