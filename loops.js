1c80af54bc0f88f036235822e9809155fa347a38
Gabrielle Piacentile <gabriellepiacentile98@gmail.com>
2020-12-13 19:58:17 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-function forLoop (array){
+/*function forLoop (array){
   for (let i = 0, i<25, i++){
     if (i === 1){
       array.push ('I am ${i} strange loop.')
@@ -7,4 +7,15 @@ function forLoop (array){
     }
   }
   return array
+}*/
+
+function forLoop(array) {
+  for (let i=0; i<25; i++) {
+    if(i === 1) {
+      array.push(`I am 1 strange loop.`)
+      } else {
+        array.push(`I am ${i} strange loops.`)
+      }
+}
+      return array;
 }
